70b26f2433ad8ee5c3af9d095f2880bb356e0430
ColePeterson-12 <816427@mystma.org>
2022-09-16 14:16:24 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-#WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(a, b, c, d, e):
+    return a + b + c + d + e
+print(topFiveMovies( "1. LOTR", " 2. Happy Gilmore", " 3.Jumanji", " 4. End Game", " 5. Deadpool"))
